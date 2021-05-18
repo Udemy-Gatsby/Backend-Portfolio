bba9961e265ebe710183edff5bfc1d9b84a83aee
wizdo-johan <soldierheart@hotmail.com>
2021-05-18 16:48:11 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1338),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ae03846e84069febea6a0124515f2843'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '650ee41c-afb3-4391-81e6-f408edcca761'
+};
